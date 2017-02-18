9af566baeb19ddc87bfc9007a122c23da6f1a67e
THUONG\IO_OI <thuongtruong2702@gmail.com>
2017-02-18 09:37:31 +0700

    Add migration

---
+++ QuanLyKho.Data/Migrations/20170218023519_InitialDB.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace QuanLyKho.Data.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Alias = table.Column<string>(maxLength: 250, nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 250, nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    DisplayOrder = table.Column<int>(nullable: true),
+                    HomeFlag = table.Column<bool>(nullable: true),
+                    Image = table.Column<string>(maxLength: 250, nullable: true),
+                    MetaDescription = table.Column<string>(maxLength: 250, nullable: true),
+                    MetaKeyWord = table.Column<string>(maxLength: 250, nullable: true),
+                    Name = table.Column<string>(maxLength: 250, nullable: false),
+                    ParentID = table.Column<int>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 250, nullable: true),
+                    UpdatedDated = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Alias = table.Column<string>(maxLength: 250, nullable: false),
+                    CategoryID = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 250, nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    HomeFlag = table.Column<bool>(nullable: true),
+                    HotFlag = table.Column<bool>(nullable: true),
+                    Image = table.Column<string>(maxLength: 250, nullable: true),
+                    MetaDescription = table.Column<string>(maxLength: 250, nullable: true),
+                    MetaKeyWord = table.Column<string>(maxLength: 250, nullable: true),
+                    MoreImages = table.Column<string>(type: "xml", nullable: true),
+                    Name = table.Column<string>(maxLength: 250, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    PromotionPrice = table.Column<decimal>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Status = table.Column<bool>(nullable: false),
+                    Tags = table.Column<string>(nullable: true),
+                    UpdatedBy = table.Column<string>(maxLength: 250, nullable: true),
+                    UpdatedDated = table.Column<DateTime>(nullable: true),
+                    ViewCount = table.Column<int>(nullable: true),
+                    Warranty = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductCategories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "ProductCategories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryID",
+                table: "Products",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+        }
+    }
+}
---
+++ QuanLyKho.Data/Migrations/QuanLyKhoDbContextModelSnapshot.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using QuanLyKho.Data;
+
+namespace QuanLyKho.Data.Migrations
+{
+    [DbContext(typeof(QuanLyKhoDbContext))]
+    partial class QuanLyKhoDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("QuanLyKho.Model.Models.Product", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Alias")
+                        .IsRequired()
+                        .HasMaxLength(250);
+
+                    b.Property<int>("CategoryID");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("CreatedBy")
+                        .HasMaxLength(250);
+
+                    b.Property<DateTime>("CreatedDate");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(500);
+
+                    b.Property<bool?>("HomeFlag");
+
+                    b.Property<bool?>("HotFlag");
+
+                    b.Property<string>("Image")
+                        .HasMaxLength(250);
+
+                    b.Property<string>("MetaDescription")
+                        .HasMaxLength(250);
+
+                    b.Property<string>("MetaKeyWord")
+                        .HasMaxLength(250);
+
+                    b.Property<string>("MoreImages")
+                        .HasColumnType("xml");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(250);
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<decimal?>("PromotionPrice");
+
+                    b.Property<int>("Quantity");
+
+                    b.Property<bool>("Status");
+
+                    b.Property<string>("Tags");
+
+                    b.Property<string>("UpdatedBy")
+                        .HasMaxLength(250);
+
+                    b.Property<DateTime?>("UpdatedDated");
+
+                    b.Property<int?>("ViewCount");
+
+                    b.Property<int?>("Warranty");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("CategoryID");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("QuanLyKho.Model.Models.ProductCategory", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Alias")
+                        .IsRequired()
+                        .HasMaxLength(250);
+
+                    b.Property<string>("CreatedBy")
+                        .HasMaxLength(250);
+
+                    b.Property<DateTime>("CreatedDate");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(500);
+
+                    b.Property<int?>("DisplayOrder");
+
+                    b.Property<bool?>("HomeFlag");
+
+                    b.Property<string>("Image")
+                        .HasMaxLength(250);
+
+                    b.Property<string>("MetaDescription")
+                        .HasMaxLength(250);
+
+                    b.Property<string>("MetaKeyWord")
+                        .HasMaxLength(250);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(250);
+
+                    b.Property<int?>("ParentID");
+
+                    b.Property<bool>("Status");
+
+                    b.Property<string>("UpdatedBy")
+                        .HasMaxLength(250);
+
+                    b.Property<DateTime?>("UpdatedDated");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("ProductCategories");
+                });
+
+            modelBuilder.Entity("QuanLyKho.Model.Models.Product", b =>
+                {
+                    b.HasOne("QuanLyKho.Model.Models.ProductCategory", "ProductCategory")
+                        .WithMany("Products")
+                        .HasForeignKey("CategoryID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
